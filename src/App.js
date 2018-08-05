2e14cec4b5861842af30dead7a11c513a60db034
QTD <tranduc811@gmail.com>
2018-08-05 19:15:49 +0700

    feat: Working with Lists and Conditions

--- src/App.js
+++ src/App.js
@@ -5,69 +5,82 @@ import Person from "./Person/Person";
 class App extends Component {
   state = {
     persons: [
-      { name: "Max 1", age: 20 },
-      { name: "Max 2", age: 30 },
-      { name: "Max 3", age: 40 }
-    ]
+      { id: 'q', name: "Max 1", age: 20 },
+      { id: 'w', name: "Max 2", age: 30 },
+      { id: 'e', name: "Max 3", age: 40 }
+    ],
+    otherState: "other value",
+    showPersons: false
   };
 
-  switchNameHandler = newName => {
-    // DON'T DO THIS: this.state.persons[0].name = 'Full name';
+  deletePersonHandler = (personIndex) => {
+    // const persons = this.state.persons.slice();
+    const persons = [...this.state.persons];
+    persons.splice(personIndex, 1);
+    this.setState({persons: persons})
+  }
+
+  nameChangedHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id === id;
+    });
+
+    // const person = Object.assign({}, this.state.persons[personIndex]);
+    const person = {
+      ...this.state.persons[personIndex]
+    };
+
+    person.name = event.target.value;
+
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+
     this.setState({
-      persons: [
-        { name: newName, age: 20 },
-        { name: "Max 2", age: 30 },
-        { name: "Max 3", age: 99 }
-      ]
+      persons: persons
     });
   };
 
-  nameChangeHandler = event => {
-    this.setState({
-      persons: [
-        { name: "Max 1", age: 20 },
-        { name: event.target.value, age: 30 },
-        { name: "Max 3", age: 40 }
-      ]
-    });
+  togglePersonsHandler = () => {
+    const doesShow = this.state.showPersons;
+    this.setState({ showPersons: !doesShow });
   };
 
   render() {
     const style = {
-      color: 'white',
-      backgroundColor: 'blue',
-      font: 'inherit',
-      border: '1px solid blue',
-      padding: '8px',
-      cursor: 'pointer'
+      color: "white",
+      backgroundColor: "blue",
+      font: "inherit",
+      border: "1px solid blue",
+      padding: "8px",
+      cursor: "pointer"
     };
+
+    let persons = null;
+
+    if (this.state.showPersons) {
+      persons = (
+        <div>
+          {this.state.persons.map((person, index) => {
+            return <Person
+              click={() => this.deletePersonHandler(index)}
+              name={person.name}
+              age={person.age}
+              key={person.id}
+              changed={(event) => this.nameChangedHandler(event, person.id)}
+            />
+          })}
+        </div>
+      );
+    }
 
     return (
       <div className="App">
         <h1>Hi</h1>
         {/* Performance impact */}
-        <button
-          style={style}
-          onClick={() => this.switchNameHandler("Full Name")}
-        >
-          Switch Name
+        <button style={style} onClick={this.togglePersonsHandler}>
+          Toggle Persons
         </button>
-        <Person
-          name={this.state.persons[0].name}
-          age={this.state.persons[0].age}
-        />
-        <Person
-          name={this.state.persons[1].name}
-          age={this.state.persons[1].age}
-        />
-        <Person
-          name={this.state.persons[2].name}
-          age={this.state.persons[2].age}
-          click={this.switchNameHandler.bind(this, "Max Again!")}
-          changed={this.nameChangeHandler}
-        >
-          My hobby: Riding
-        </Person>
+        {persons}
       </div>
     );
   }
